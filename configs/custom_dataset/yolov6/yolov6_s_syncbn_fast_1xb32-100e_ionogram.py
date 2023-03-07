6a933cf22e06d07d51aede5d180fe0ccf2fd1cf2
VoyagerXvoyagerx <19211416@bjtu.edu.cn>
2023-03-07 13:30:37 +0800

    Beautify config for yolov6_s

--- configs/custom_dataset/yolov6/yolov6_s_syncbn_fast_1xb32-100e_ionogram.py
+++ configs/custom_dataset/yolov6/yolov6_s_syncbn_fast_1xb32-100e_ionogram.py
@@ -1,33 +1,39 @@
 _base_ = '../../yolov6/yolov6_s_syncbn_fast_8xb32-400e_coco.py'
 
-max_epochs = 100  # 训练的最大 epoch
-data_root = './Iono4311/'  # 数据集目录的绝对路径
-
-
+# ========================modified parameters======================
+# -----data related-----
+data_root = './Iono4311/'
 work_dir = './work_dirs/yolov6_s_100e'
-load_from = './work_dirs/yolov6_s_syncbn_fast_8xb32-400e_coco_20221102_203035-932e1d91.pth'  # noqa
-
-# 根据自己的 GPU 情况，修改 batch size，YOLOv6-s 默认为 8卡 x 32bs
-train_batch_size_per_gpu = 32
-train_num_workers = 4  # 推荐使用 train_num_workers = nGPU x 4
-
-save_epoch_intervals = 2  
-
-#  base_lr_default * (your_bs 32 / default_bs (8 x 32))
-base_lr = _base_.base_lr / 8
-
 class_name = ('E', 'Es-l', 'Es-c', 'F1', 'F2', 'Spread-F')
 num_classes = len(class_name)
 metainfo = dict(
     classes = class_name,
-    palette = [(250, 165, 30), (120, 69, 125), (53, 125, 34), (0, 11, 123), (130, 20, 12), (120, 121, 80)]  # 画图时候的颜色，随便设置即可
+    palette = [(250, 165, 30), (120, 69, 125), (53, 125, 34),
+               (0, 11, 123), (130, 20, 12), (120, 121, 80)]
 )
 
+train_batch_size_per_gpu = 32
+train_num_workers = 8
+
+# -----train val related-----
+load_from = 'https://download.openmmlab.com/mmyolo/v0/yolov6/yolov6_s_syncbn_fast_8xb32-400e_coco/yolov6_s_syncbn_fast_8xb32-400e_coco_20221102_203035-932e1d91.pth'  # noqa
+#  base_lr_default * (your_bs 32 / default_bs (8 x 32))
+base_lr = _base_.base_lr * train_batch_size_per_gpu / (8 * 32)
+max_epochs = 100
+save_epoch_intervals = 10
+val_begin = 20
+max_keep_ckpts = 1
+log_interval = 50
+visualizer = dict(
+    vis_backends=[dict(type='LocalVisBackend'),
+                  dict(type='WandbVisBackend'), dict(type='TensorboardVisBackend')])
+
+# ==================== Unmodified in most cases ===================
 train_cfg = dict(
     max_epochs=max_epochs,
-    val_begin=2,  
+    val_begin=val_begin,  
     val_interval=save_epoch_intervals,  
-    # dynamic_intervals=[(max_epochs - _base_.num_last_epochs, 1)]
+    dynamic_intervals=None
 )
 
 model = dict(
@@ -77,10 +83,10 @@ default_hooks = dict(
     checkpoint=dict(
         type='CheckpointHook',
         interval=save_epoch_intervals,
-        max_keep_ckpts=1,
+        max_keep_ckpts=max_keep_ckpts,
         save_best='auto'),
     param_scheduler=dict(max_epochs=max_epochs),
-    logger=dict(type='LoggerHook', interval=50))
+    logger=dict(type='LoggerHook', interval=log_interval))
 
 custom_hooks = [
     dict(
@@ -95,4 +101,3 @@ custom_hooks = [
         switch_epoch=max_epochs - _base_.num_last_epochs,
         switch_pipeline=_base_.train_pipeline_stage2)
 ]
-visualizer = dict(vis_backends=[dict(type='LocalVisBackend'), dict(type='WandbVisBackend')])
